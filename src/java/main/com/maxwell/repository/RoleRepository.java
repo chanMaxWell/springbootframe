f67577c9de64b19e355bc2653a27cacc6c807ab4
huopo <chan_MaxWell@163.com>
2018-06-12 09:46:28 +0800

    shiro配置中提交

--- src/java/main/com/maxwell/repository/RoleRepository.java
+++ src/java/main/com/maxwell/repository/RoleRepository.java
@@ -1,7 +1,15 @@
 package com.maxwell.repository;
 
+import com.maxwell.entity.Role;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * Created by Fhcard on 2018/6/11.
  */
+@Repository
 public interface RoleRepository {
+
+    List<Role> findRoleByUserId(Long userId);
 }
--- src/java/main/com/maxwell/service/impl/RoleServiceImpl.java
+++ src/java/main/com/maxwell/service/impl/RoleServiceImpl.java
@@ -1,11 +1,24 @@
 package com.maxwell.service.impl;
 
+import com.maxwell.entity.Role;
+import com.maxwell.repository.RoleRepository;
 import com.maxwell.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Fhcard on 2018/6/11.
  */
 @Service
 public class RoleServiceImpl implements RoleService{
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public List<Role> findRoleByUserId(Long userId) {
+        return roleRepository.findRoleByUserId(userId);
+    }
 }
